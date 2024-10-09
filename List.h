437badbcf26670908280a51a4027274a4e2e2295
Maria Rodriguez Hernandez <mrodher@upv.edu.es>
2024-10-09 19:40:09 +0200

    Añadida interfaz genérica List

--- List.h
+++ List.h
@@ -4,21 +4,21 @@
 template <typename T>
 class List{
 	public:
-		void insert(int pos, T e);
+		void insert(int pos, T e); //inserta e en posición pos
 
-		void append(T e);
+		void append(T e); //inserta e al final de la lista
 		
-		void prepend(T e);
+		void prepend(T e); //inserta e al principio de la lista
 
-		T remove (int pos);
+		T remove (int pos); //delete y devuelve elem. situado en pos
 
-		T get(int pos);
+		T get(int pos); //devuelve elemento situado en pos
 
-		int search (T e);
+		int search (T e); //devuelve pos encuentra primera vez e
 
-		bool empty();
+		bool empty(); //indica si lista está vacía
 
-		int size();
+		int size(); //devuelve num elementos lista
 };
 
 #endif
