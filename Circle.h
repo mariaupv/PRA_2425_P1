a8c3128ef0d78660d20675d61610c6fbac29cdc9
Maria Rodriguez Hernandez <mrodher@upv.edu.es>
2024-10-23 23:47:57 +0200

    Añadido fichero de cabeceras Circle.h

---
+++ Circle.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Shape.h"
+using namespace std;
+
+class Circle : public Shape{
+    private :
+        Point2D center;
+        double radius;
+    
+    public:
+        Circle();
+        Circle(string color, Point2D center, double radius);
+        Point2D get_center() const;
+        void set_center(Point2D p);
+        double get_radius() const;
+        void set_radius(double r);
+        friend ostream& operator<< (ostream &out, const Circle &c);
+        
+        double area() const override;
+        double perimeter() const override;
+        void translate(double incx, double incY) override;
+        void print() override;
+};
