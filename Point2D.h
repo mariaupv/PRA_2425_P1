fef22d286c5d47947c6764e634359ba86c826f65
Maria Rodriguez Hernandez <mrodher@upv.edu.es>
2024-10-23 22:49:48 +0200

    Añadido fichero de declaracion de la clase Point2D

--- Point2D.h
+++ Point2D.h
@@ -8,7 +8,7 @@ class Point2D{
 		double x;
 		double y;
 
-		Point2D(double x=0, double y=0);
+		Point2D(double x=0.0, double y=0.0);
 
 		static double distance(const Point2D &a, const Point2D &b);
 
