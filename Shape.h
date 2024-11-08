76918ef1372788e0c5c8ded0a7063b867816cfc1
Maria Rodriguez Hernandez <mrodher@upv.edu.es>
2024-11-08 11:44:03 +0100

    Añadido declaración de clase Shape.h

--- Shape.h
+++ Shape.h
@@ -10,14 +10,14 @@ class Shape{
 
     public:
 
-        Shape();
-        Shape(string color);
-        string get_color() const;
-        void set_color(string c);
-        virtual double area() const = 0;
-        virtual double perimeter() const = 0;
-        virtual void translate(double incx, double incY) = 0;
-        virtual void print() = 0;
+        Shape(); // Crea figura de color rojo
+        Shape(string color); // Crea figura de un color específico
+        string get_color() const; // Devuelve el color de relleno de la figura
+        void set_color(string c); // Modifica color de relleno
+        virtual double area() const = 0; // Calcula área de la figura
+        virtual double perimeter() const = 0; // Calcula perímetro de la figura
+        virtual void translate(double incx, double incY) = 0; // traslada figura sobre espacio de representación
+        virtual void print() = 0; // Imprime info básica de la figura
 };
 #endif
 
