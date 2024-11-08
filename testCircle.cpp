ec20ffd4814813431a1741582999221d3f4578c5
Maria Rodriguez Hernandez <mrodher@upv.edu.es>
2024-11-08 13:03:30 +0100

    Añadido main de testCircle.cpp

--- testCircle.cpp
+++ testCircle.cpp
@@ -6,7 +6,7 @@ int main(){
     Circle c1;
     Circle c2("green", Point2D(1,1), 2);
     std::cout << "c1 = " << c1 << std::endl;
-    std::cout << "c2 = " << c2 << std::endl; 
+    std::cout << "c2 = " << c2 << std::endl;   
     std::cout << std::endl;
 
     c1.set_center(c2.get_center());
